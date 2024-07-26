f9184b6ca219771c66f314e7bdd5723ece275ae6
badac <badaclock@gmail.com>
2024-07-27 04:20:47 +0900

    Feat: .gitkeep 삭제, main.tst에 a변수 제목거

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
-const a: number = '3123';
-console.log(a);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
