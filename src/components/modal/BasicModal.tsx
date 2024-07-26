b2419223b367cab4057a746af834e78a2cae1a7a
badac <badaclock@gmail.com>
2024-07-27 06:15:27 +0900

    design: 모달UI 구현 - BasicModal.tsx: 기본 모달 UI 구현 - MobileModal.tsx: 모바일 모달 UI 구현 Github issue #13

--- src/components/modal/BasicModal.tsx
+++ src/components/modal/BasicModal.tsx
@@ -1,44 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import CloseIcon from '@mui/icons-material/Close';
+import * as Styled from './BasicModal.style';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  color: 'text.primary',
-  p: 4,
-};
+interface BasicModalProps {
+  nav?: JSX.Element;
+  children?: React.ReactNode[];
+}
 
-export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
+export default function BasicModal({ nav, children }: BasicModalProps) {
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+      <Modal open={open} onClose={handleClose}>
+        <Styled.ModalContent>
+          <Styled.closeButton onClick={handleClose}>
+            <CloseIcon />
+          </Styled.closeButton>
+          <Styled.ModalChildrenContent>
+            {Array.isArray(children)
+              ? children.map((child, index) => <Styled.Wrapper key={index}>{child}</Styled.Wrapper>)
+              : children}
+          </Styled.ModalChildrenContent>
+          {nav || ''}
+        </Styled.ModalContent>
       </Modal>
     </div>
   );
