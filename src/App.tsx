6fb57e538f70f0fd66741fb8223e486645fd82a0
badac <badaclock@gmail.com>
2024-07-29 23:57:00 +0900

    Feat: 네비게이션바 라우터 추가 - 네비게이션바에 라우터 추가 - src/router.tsx 파일에 라우터 추가 - src/components/nav/navLinks.tsx 파일 추가 Github issue #36

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import NavBar from './components/nav/NavBar';
-import Dashboard from './pages/Dashboard';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Dashboard />
       <NavBar />
+      <Outlet />
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
