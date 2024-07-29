0eb96f44c4aad12ba6ddc650601406839cb6f5d4
jizero <jizero27@gmail.com>
2024-07-29 11:02:34 +0900

    Design : [버튼 컴포넌트] 비활성화 버튼 색상 변경 , onClick 속성 추가 비활성화 버튼 컬러 와이어프레임에 컬러로 변경 onClick 속성 추가

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,26 +1,29 @@
 import StyledButton from './Button.styled';
+import { MouseEventHandler } from 'react';
 //btnType의 경우 outlined로 타입을 넣어주지 않을 경우 기본 버튼은 primary
 //size의 경우 sm:기본 버튼, md: 로그인 버튼 
-//<Btn size='md' label='확인'/>
-//<Btn round size='sm' label='신청하기'/> round 속성은 동그란 모양의 버튼
-//<Btn disabled round size='sm' label='비활성화'/>
+//<Btn label='활성화'/> 예) 기본 버튼
+//<Btn round='true' label='신청하기'/> 예) 동그란 모양의 버튼
+//<Btn disabled label='비활성화'/> 예) 비활성화 버튼
 
 type ButtonProps = {
   label: string
   btntype?: string
-  size: 'sm' | 'md'
-  round?:boolean
+  btnsize?:string
+  round?:string
   disabled?:boolean
+  onClick?:MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Btn({btntype, size, label, round=false, disabled=false} : ButtonProps){
+export default function Btn({btntype, btnsize, label, round, disabled=false, onClick} : ButtonProps){
   return (
     <StyledButton
       variant="outlined"
       btntype={btntype}
-      size={size}
+      btnsize={btnsize}
       round={round}
       disabled={disabled}
+      onClick={onClick}
     >
       {label}
     </StyledButton>
