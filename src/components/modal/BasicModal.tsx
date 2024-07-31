bd7d700612848fc9187f0d8035faa615bde520a7
badac <badaclock@gmail.com>
2024-07-31 15:23:36 +0900

    Fix: 모달 컴포넌트 수정, 기본 닫기 버튼, 기본 열기 버튼 제거

--- src/components/modal/BasicModal.tsx
+++ src/components/modal/BasicModal.tsx
@@ -1,12 +1,9 @@
-import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import NavBar from '../nav/NavBar';
-import { IF } from '../../utils/IFElse';
 import React from 'react';
-import { CloseButton } from './CloseButton';
-import { useBasicModal } from './useBasicModal';
+import { ModalProps } from '@mui/material';
 
-interface BasicModalProps {
+interface BasicModalProps extends Omit<ModalProps, 'children'> {
   children?: React.ReactElement;
   modalOpenButton?: React.ReactElement;
   modalCloseButton?: React.ReactNode;
@@ -18,28 +15,13 @@ export default function BasicModal({
   modalCloseButton,
   ...props
 }: BasicModalProps) {
-  const { open, handleOpen, handleClose } = useBasicModal();
-
   return (
     <div>
-      <IF condition={!!modalOpenButton}>
-        <IF.Then>{modalOpenButton}</IF.Then>
-        <IF.Else>
-          <Button onClick={handleOpen}>Open modal</Button>
-        </IF.Else>
-      </IF>
-      <Modal {...props} open={open} onClose={handleClose}>
+      {modalOpenButton}
+      <Modal {...props}>
         <>
-          <IF condition={!!children}>
-            <IF.Then>{children}</IF.Then>
-            <IF.Else>""</IF.Else>
-          </IF>
-          <IF condition={!!modalCloseButton}>
-            <IF.Then>{modalCloseButton}</IF.Then>
-            <IF.Else>
-              <CloseButton handleClose={handleClose} />
-            </IF.Else>
-          </IF>
+          {children}
+          {modalCloseButton}
           <NavBar />
         </>
       </Modal>
