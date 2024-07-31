f4826cbf0a7a82fff7f4926829c78a78c784d1d0
jizero <jizero27@gmail.com>
2024-07-31 10:14:17 +0900

    datepicker 컴포넌트 리팩토링 mui datepicker props 상속시켜서 사용할 수 있도록 함

--- src/components/datepicker/DatePicker.tsx
+++ src/components/datepicker/DatePicker.tsx
@@ -2,26 +2,41 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Separator, DatePickerContainer, DTPstyle } from './DatePicker.styled';
 import { DatePicker } from '@mui/x-date-pickers';
+import { DatePickerProps } from '@mui/x-date-pickers';
+import { Dayjs } from 'dayjs';
+import { useState } from 'react';
 
-type pickerProps = {
+type pickerProps = DatePickerProps<Dayjs, false>& {
   dateType : 'single' | 'range'
 }
 
-export default function SDataPicker({dateType}:pickerProps){
+export default function SDataPicker({dateType, ...props}:pickerProps){
+  const [startDate, setStartDate] = useState<Dayjs | null>(null)
+  const [endDate, setEndDate] = useState<Dayjs | null>(null)
+
   switch(dateType){
     case 'single':
       return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker slotProps={DTPstyle}/>
+            <DatePicker slotProps={DTPstyle} {...props}/>
         </LocalizationProvider>
       )
     case 'range':
       return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePickerContainer>
-              <DatePicker slotProps={DTPstyle}/>
+              <DatePicker 
+              slotProps={DTPstyle} 
+              value={startDate}
+              onChange={(newvalue) => setStartDate(newvalue)}
+
+              {...props}/>
             <Separator>-</Separator>
-            <DatePicker slotProps={DTPstyle}/>
+            <DatePicker 
+            slotProps={DTPstyle}
+            value={endDate}
+            onChange={(newvalue) => setEndDate(newvalue)} 
+            {...props}/>
           </DatePickerContainer>
         </LocalizationProvider>
       )
