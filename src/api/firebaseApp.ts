ec214c9360624b1b6cce5f86019bcacfe6e5ec34
jizero <jizero27@gmail.com>
2024-08-06 11:29:20 +0900

    Feat : 급여정산 페이지 api 연동 급여 정산 페이지, 급여 내 페이지 파이어베이스 데이터 연동 - react-query 활용 - 발표일에 맞춰 급여일 25일 -> 12일로 변경 (정정신청은 10일까지로 데이터   변경)
    [파이어베이스 관련]
    - utils/vite-env.d.ts 파일에 DATABASE_URL readonly로 추가
    - api/firebaseconfig 파일에 databaseURL 추가
    
    [페이지명]
    -페이지 컴포넌트의 경우 네임 뒤에 Page를 붙여 구분할 수 있도록함
    Github issue #83

--- src/api/firebaseApp.ts
+++ src/api/firebaseApp.ts
@@ -1,12 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 import { firebaseConfig } from './firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 const firebaseDB = getDatabase(app);
 
-export { app, auth, db, firebaseDB };
+export { app, auth, firebaseDB };
