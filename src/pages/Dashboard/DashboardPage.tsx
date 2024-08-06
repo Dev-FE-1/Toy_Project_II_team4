45f7938a87d91ccf22886d5a2cecbde5260b440f
Mark Kwon <redhero8830@gmail.com>
2024-08-06 22:58:34 +0900

    Style: 불필요 주석 삭제
    Github issue #101

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,24 +1,11 @@
 import ProfileCard from './ProfileCard';
 import CalendarCard from './CalendarCard';
-import NoticeCard from '../salaryList/NoticeCard';
-import dayjs from 'dayjs';
-import { useNavigate } from 'react-router-dom';
 import SalaryCard from './SalaryCard';
 // import { useSelector } from 'react-redux';
 // import { RootState } from '../../store/store';
 // import { ISchedule } from '../../slices/scheduleSlice';
 
 function Dashboard() {
-  const currentDate = new Date();
-  const DueDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 25);
-  const originDate = dayjs(DueDate);
-  const finalMonth = originDate.format('MM월 ');
-  const finalDay = originDate.format('DD일');
-
-  const navigate = useNavigate();
-
-  const goToSalaryPage = () => navigate(`/salary-detail/3`);
-
   // const schedules = useSelector((state: RootState) => state.schedules.schedules);
   // 오늘의 스케줄 필터링
   // const todaySchedules = schedules.filter(
@@ -29,13 +16,6 @@ function Dashboard() {
     <>
       <ProfileCard />
       <CalendarCard />
-      <NoticeCard
-        date={finalMonth}
-        day={finalDay}
-        button={true}
-        label={<h5>급여명세서 조회</h5>}
-        onClick={goToSalaryPage}
-      />
       <SalaryCard />
     </>
   );
