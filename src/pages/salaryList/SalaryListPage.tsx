7c477ac88a462045b339d5701b64cd2bc37e52b5
jizero <jizero27@gmail.com>
2024-08-07 22:16:47 +0900

    Feat : 목록 페이지 코드 수정 Number로 형변환하던 구문을 단항 플러스 연산자로 바꿔서 형변환 시키도록 함 목록을 클릭했을 때 빠른 급여명세서 조회를 위해 set, map을 활용하라는 리뷰 반영 Github issue #109

--- src/pages/salaryList/SalaryListPage.tsx
+++ src/pages/salaryList/SalaryListPage.tsx
@@ -1,11 +1,11 @@
-import SelectBox from "../../components/selectBox/SelectBox";
-import Btn from "../../components/button/Button";
+import SelectBox from '../../components/selectBox/SelectBox';
+import Btn from '../../components/button/Button';
 import * as Styled from './SalaryList.style';
-import { useNavigate } from "react-router-dom";
-import NoticeCard from "./NoticeCard";
-import useSalaryDetails from "./useSalaryDetails";
-import Heading from "../../components/Heading/Heading";
-import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import NoticeCard from './NoticeCard';
+import useSalaryDetails from './useSalaryDetails';
+import Heading from '../../components/Heading/Heading';
+import { useState } from 'react';
 
 const years = [
   { value: '2022', text: '2022' },
@@ -13,72 +13,89 @@ const years = [
   { value: '2024', text: '2024' },
 ];
 
-export default function SalaryListPage(){
-  const navigate = useNavigate()
-  const userId = "sajo1234567"
-  const [selectedYear, setSelectedYear] = useState<number>(2024)
-  const {data, error, isLoading} = useSalaryDetails()
+export default function SalaryListPage() {
+  const navigate = useNavigate();
+  const userId = 'sajo1234567';
+  const [selectedYear, setSelectedYear] = useState<number>(2024);
+  const { data, error, isLoading } = useSalaryDetails();
 
-  if (isLoading) {return <div>Loading...</div>}
-  if (error) {return <div>Error: {error.message}</div>}
-
-  const salaryList = data?.salaryDetails[userId] || [] 
-  const filteredItem = salaryList.filter((item) => Number(item.payday.slice(0,4)) === Number(selectedYear))
-  const sortedData = [...filteredItem].sort((a,b) => b.id-a.id)
-  const latestSalaryList = [...salaryList].sort((a, b) => new Date(b.payday).getTime() - new Date(a.payday).getTime());
-  const latestData = latestSalaryList.length > 0 ? [latestSalaryList[0]] : [];
-
-  const handleApplicationBtn = (id:number) => {
-    if(sortedData.find((item) => item.id === id)){
-      navigate(`/salary-detail/${id}`)
-    }else{
-      navigate('/payments')
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
-  return(
+  const salaryList = data?.salaryDetails[userId] || [];
+
+  //목록에 활용되는 데이터
+  const filteredItem = salaryList.filter((item) => +item.payday.slice(0, 4) === +selectedYear);
+  const sortedData = [...filteredItem].sort((a, b) => b.id - a.id);
+
+  //카드에 활용되는 데이터
+  const latestSalaryList = [...salaryList].sort(
+    (a, b) => new Date(b.payday).getTime() - new Date(a.payday).getTime()
+  );
+  const latestData = latestSalaryList.length > 0 ? [latestSalaryList[0]] : [];
+
+  const salaryIdSet = new Set(sortedData.map((item) => item.id));
+  const handleApplicationBtn = (id: number) => {
+    if (salaryIdSet.has(id)) {
+      navigate(`/salary-detail/${id}`);
+    } else {
+      navigate('/payments');
+    }
+  };
+
+  return (
     <Styled.Salary>
-      <Heading title="급여정산"/>
-      <NoticeCard salaryList={latestData}/>
-        <Styled.YearSelect>
-        <SelectBox 
-          labelId="SalaryYear" 
-          id="year-select" 
-          label="year" 
+      <Heading title="급여정산" />
+      <NoticeCard salaryList={latestData} />
+      <Styled.YearSelect>
+        <SelectBox
+          labelId="SalaryYear"
+          id="year-select"
+          label="year"
           menuItems={years}
           value={selectedYear}
-          onChange={(e) => setSelectedYear(Number(e.target.value))
-          }
-          sx={{'& .MuiInputLabel-root': {
-              fontSize: 'var(--font-size-primary)', 
+          onChange={(e) => setSelectedYear(Number(e.target.value))}
+          sx={{
+            '& .MuiInputLabel-root': {
+              fontSize: 'var(--font-size-primary)',
             },
             '& .MuiSelect-select': {
-              fontSize: 'var(--font-size-small)', 
+              fontSize: 'var(--font-size-small)',
             },
             '& .MuiSelect-icon': {
               fontSize: '2rem',
               right: '2rem',
               transform: 'translateY(-50%)',
-              top: '40%'
-            }
+              top: '40%',
+            },
           }}
         />
       </Styled.YearSelect>
-        {sortedData.map((el)=>
-          (<Styled.ListCardBox key={el.id} $state={el.state} 
-            onClick={()=>{handleApplicationBtn(el.id)}}>
-            <Styled.List $state={el.state}>
+      {sortedData.map((el) => (
+        <Styled.ListCardBox
+          key={el.id}
+          $state={el.state}
+          onClick={() => {
+            handleApplicationBtn(el.id);
+          }}
+        >
+          <Styled.List $state={el.state}>
             <span className="title">{el.title}</span>
             <span className="date">{el.state === true ? '지급예정' : el.payday}</span>
-            </Styled.List>
-            <Styled.Btn>
-              {el.state === true ? 
-              <Btn round ='true' btntype='outlined' size='lg' label='신청가능'/> 
-              : 
-              <Btn round='true' disabled size='lg' label='지급완료'/> 
-              }
-            </Styled.Btn>
-        </Styled.ListCardBox>))}
+          </Styled.List>
+          <Styled.Btn>
+            {el.state === true ? (
+              <Btn round="true" btntype="outlined" size="lg" label="신청가능" />
+            ) : (
+              <Btn round="true" disabled size="lg" label="지급완료" />
+            )}
+          </Styled.Btn>
+        </Styled.ListCardBox>
+      ))}
     </Styled.Salary>
-    )
+  );
 }
