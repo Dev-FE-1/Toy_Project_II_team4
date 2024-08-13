5405319a94a7d027fd54f0a1eeac5ef02f440067
badac <badaclock@gmail.com>
2024-08-13 15:45:45 +0900

    feat: StoryBook 세팅 및 NavBar 스토리 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2021: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
-    //'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', '**/*.html', '.config/', '.github/', 'document/'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'prettier', '@typescript-eslint/eslint-plugin'],
