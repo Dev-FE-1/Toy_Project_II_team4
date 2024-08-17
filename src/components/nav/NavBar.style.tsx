1130e71a00bfafb88a496380c1fd462d823ab683
badac <badaclock@gmail.com>
2024-08-17 13:48:22 +0900

    refactor: Refactor NavBar component - React Router Dom에서 제공하는  NavLink 를 이용해서 포커스 - 네비바 컴포넌트를 리펙토링 - NavButton 컴포넌트와 관련 컴포넌트 삭제 - 네비바를 버튼 컴포넌트가 아닌 ul, li를 이용해서 재작성

--- src/components/nav/NavBar.style.tsx
+++ src/components/nav/NavBar.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const NavBarWrapper = styled.nav`
+export const NavBarContainer = styled.nav`
   position: fixed;
   max-width: 568px;
   margin: 0 auto;
@@ -9,17 +9,69 @@ export const NavBarWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 8rem;
-`;
 
-export const MyToolbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: #ffff;
-  flex-grow: 1, calc(100% / 5);
-  padding-right: 0;
-  padding-left: 0;
-  @media (min-width: 600px) {
-    padding-right: 0px;
-    padding-left: 0px;
+  & ul.navbar-ul-items {
+    display: flex;
+    background-color: #ffff;
+    flex-grow: 1, calc(100% / 5);
+    padding-right: 0;
+    padding-left: 0;
+
+    & li.navbar-li-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      color: black;
+      transition: all 0.3s ease;
+      flex: 1;
+      height: 80px;
+      min-width: 0;
+
+      & a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: inherit;
+        text-decoration: none;
+        width: 100%;
+        height: 100%;
+      }
+
+      &:hover,
+      & a.active {
+        color: var(--color-pri);
+      }
+
+      & .icon-box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 8px;
+
+        & svg {
+          width: 3rem;
+          height: 3rem;
+        }
+
+        & .home-icon {
+          width: 31px;
+          height: 31px;
+        }
+      }
+
+      & .text-box {
+        font-size: var(--font-size-primary);
+        font-weight: bold;
+        text-align: center;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+        max-width: 100%;
+        font-weight: var(--font-weight-semibold);
+      }
+    }
   }
 `;
